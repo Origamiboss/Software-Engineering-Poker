0a430b8cf3467336bacc7e4617d45890379d5ad8
Origamiboss <vincentclimer@gmail.com>
2024-11-22 21:45:04 -0600

    Create GameData.java

---
+++ sweProject/GameData.java
@@ -0,0 +1,27 @@
+package sweProject;
+
+public class GameData {
+	private int bettedMoney;
+	private int cardsSwapped;
+	private int totalMoneyAmount;
+	
+	public int getBettedMoney() {
+		return bettedMoney;
+	}
+	public int getCardsSwapped() {
+		return cardsSwapped;
+	}
+	public int getTotalMoneyAmount() {
+		return totalMoneyAmount;
+	}
+	public void setBettedMoney(int t) {
+		bettedMoney = t;
+	}
+	public void setCardsSwapped(int t) {
+		cardsSwapped = t;
+	}
+	public void setTotalMoneyAmount(int t) {
+		totalMoneyAmount = t;
+	}
+	
+}
