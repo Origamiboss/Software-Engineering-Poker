14e0c075a18066340c381571629b5cdc97dba59c
Landonie314 <trumpet.landon@gmail.com>
2024-12-02 14:26:45 -0600

    Added tests for showPages

--- sweProject/MainControlTest.java
+++ sweProject/MainControlTest.java
@@ -1,11 +1,25 @@
 package sweProject;
 
 import static org.junit.Assert.*;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainControlTest {
-
+	private MainControl mc;
+    private ByteArrayOutputStream outputStreamCaptor;
+	
+	
+	@Before
+	public void setUp() throws Exception 
+	{
+		mc = new MainControl();
+		 // Setup to capture System.out output
+        outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+	}
+	
 	@Test
 	public void testMainControl() {
 		fail("Not yet implemented");
@@ -13,27 +27,54 @@ public class MainControlTest {
 
 	@Test
 	public void testOpenInitial() {
-		fail("Not yet implemented");
+		// Call the method under test
+        mc.openInitial();
+
+        // Verify that showPage was called with the correct argument ("Initial")
+        String expectedOutput = "Displaying page: Initial\n";
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
 	}
+
 
 	@Test
 	public void testOpenMainPage() {
-		fail("Not yet implemented");
+		// Call the method under test
+        mc.openMainPage();
+
+        // Verify that showPage was called with the correct argument ("Initial")
+        String expectedOutput = "Displaying page: Main\n";
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
 	}
 
 	@Test
 	public void testOpenNewAccount() {
-		fail("Not yet implemented");
-	}
+		// Call the method under test
+        mc.openNewAccount();
+
+        // Verify that showPage was called with the correct argument ("Initial")
+        String expectedOutput = "Displaying page: Account\n";
+        assertEquals(expectedOutput, outputStreamCaptor.toString());	
+     }
 
 	@Test
 	public void testOpenGameClient() {
-		fail("Not yet implemented");
+
+		// Call the method under test
+        mc.openGameClient();
+
+        // Verify that showPage was called with the correct argument ("Initial")
+        String expectedOutput = "Displaying page: GameClient\n";
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
 	}
 
 	@Test
 	public void testOpenLogin() {
-		fail("Not yet implemented");
+		// Call the method under test
+        mc.openLogin();
+
+        // Verify that showPage was called with the correct argument ("Initial")
+        String expectedOutput = "Displaying page: Login\n";
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
 	}
 
 	@Test
