e5fdea135f3777c89cb04f276f5e4cb2d33ff979
Landonie314 <trumpet.landon@gmail.com>
2024-12-01 23:30:31 -0600

    Start of JUnit test files
    Creation of JUnit test files and addition of JUnit4 library to the project.

---
+++ NewAccountUI/NewAccountControllerTest.java
@@ -0,0 +1,24 @@
+package NewAccountUI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewAccountControllerTest {
+
+	@Test
+	public void testNewAccountController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenInitialPanel() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ sweProject/MainControlTest.java
@@ -0,0 +1,69 @@
+package sweProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainControlTest {
+
+	@Test
+	public void testMainControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenInitial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenMainPage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenNewAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenGameClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHostGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidIp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
